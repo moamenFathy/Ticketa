8f647ad7a7a9e672a39e9239beb301cca56a1f15
moamenFathy <moamenmahmoud118@gmail.com>
2026-05-03 23:06:43 +0300

    Add WeatherForecast API and configure project setup
    - Implemented `WeatherForecastController` with a sample API.
    - Configured application pipeline in `Program.cs` with CORS, OpenAPI, and middleware.
    - Added `launchSettings.json` for development profiles.
    - Created `Ticketa.API.csproj` with dependencies and project references.
    - Added `Ticketa.API.http` for API testing.
    - Defined `WeatherForecast` model in `WeatherForecast.cs`.
    - Configured `appsettings.json` with logging, DB connection, and TMDB API.

---
+++ Ticketa.API/Program.cs
@@ -0,0 +1,42 @@
+using Scalar.AspNetCore;
+using Ticketa.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddTicketaInfrastructure(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.SetIsOriginAllowed(_ => true)
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.MapOpenApi();
+  app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
