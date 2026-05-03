51568775f7cea48c8573c32697a491d15970c818
moamenFathy <moamenmahmoud118@gmail.com>
2026-05-03 19:08:02 +0300

    Refactor DI setup; add Import button and update icons
    Refactor infrastructure service registration into AddTicketaInfrastructure extension for cleaner DI in Program.cs. Add Microsoft.AspNetCore.App reference and Polly package. Update Movies page with Import button and SVG icon. Improve sidebar icon formatting in admin layout.

--- apps/server/Ticketa.Web/Program.cs
+++ apps/server/Ticketa.Web/Program.cs
@@ -1,66 +1,17 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Polly;
-using Ticketa.Core.Entities;
-using Ticketa.Core.Interfaces;
-using Ticketa.Core.Interfaces.IServices;
-using Ticketa.Core.Interfaces.Services;
-using Ticketa.Core.Mapping;
-using Ticketa.Core.Settings;
-using Ticketa.Infrastructure.Data;
-using Ticketa.Infrastructure.Repositories;
-using Ticketa.Infrastructure.Service;
+using Ticketa.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>((opt) =>
-{
-  opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
-{
-  opt.User.RequireUniqueEmail = true;
-  opt.SignIn.RequireConfirmedEmail = true;
-}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IMoviesService, MoviesService>();
-builder.Services.AddScoped<IShowtimeService, ShowtimeService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-// Email settings
-builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
-builder.Services.AddScoped(sp => sp.GetRequiredService<IOptions<EmailSettings>>().Value);
-
-builder.Services.AddOptions();
-builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
-
-// Your service
-builder.Services.AddScoped<IEmailService, EmailService>();
-
-
-builder.Services.AddAutoMapper(cfg =>
-{
-  cfg.AddMaps(typeof(MovieProfile).Assembly);
-});
-
-builder.Services.AddHttpClient<ITmdbService, TmdbService>(opt =>
-{
-  opt.BaseAddress = new Uri("https://api.themoviedb.org/3/");
-  opt.Timeout = TimeSpan.FromSeconds(10);
-}).AddTransientHttpErrorPolicy(policy =>
-    policy.WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))));
+builder.Services.AddTicketaInfrastructure(builder.Configuration);
 
 builder.Services.ConfigureApplicationCookie(opt =>
 {
   opt.LoginPath = "/Auth/Login";
 });
-
 
 var app = builder.Build();
 
