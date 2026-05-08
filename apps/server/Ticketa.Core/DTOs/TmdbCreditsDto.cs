20ec53906fe74a4b7aabb8a0696044c43e1eb333
moamenFathy <moamenmahmoud118@gmail.com>
2026-05-08 16:58:51 +0300

    Add DTOs for TMDb cast and credits data
    Added `TmdbCastMemberDto` to represent cast member details, including properties like `Id`, `Name`, `Character`, `ProfilePath`, and `Order`. Mapped properties to JSON using `JsonPropertyName`.
    
    Added `TmdbCreditsDto` to represent credits data with a `Cast` property as a list of `TmdbCastMember` objects, also mapped to JSON.

---
+++ apps/server/Ticketa.Core/DTOs/TmdbCreditsDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ticketa.Core.DTOs
+{
+  public class TmdbCreditsDto
+  {
+    [JsonPropertyName("cast")]
+    public List<TmdbCastMember> Cast { get; set; } = [];
+  }
+}
