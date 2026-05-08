6bb13ea5aae2ee8c01c4c1488034c2f1b9efb8aa
moamenFathy <moamenmahmoud118@gmail.com>
2026-05-08 16:58:28 +0300

    Add Cast entity and migration for database schema
    Added a new `Cast` entity with properties and relationships to the `Movie` entity. Updated `ApplicationDbContext` to include `DbSet<Cast>`. Created migration `20260508135044_addCastTableToDb` to add the `Casts` table with a foreign key to `Movies`. Updated `ApplicationDbContextModelSnapshot` to reflect these changes.

---
+++ apps/server/Ticketa.Core/Entities/Cast.cs
@@ -0,0 +1,14 @@
+﻿namespace Ticketa.Core.Entities
+{
+  public class Cast
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? ProfilePath { get; set; }
+    public string Character { get; set; } = string.Empty;
+    public int Order { get; set; }
+
+    public int MovieId { get; set; }
+    public Movie Movie { get; set; } = null!;
+  }
+}
---
+++ apps/server/Ticketa.Infrastructure/Migrations/20260508135044_addCastTableToDb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCastTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Casts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfilePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Character = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Casts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Casts_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Casts_MovieId",
+                table: "Casts",
+                column: "MovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Casts");
+        }
+    }
+}
