85517fd0eaa5e6553d54be31d4d9969ffebe5da4
moamenFathy <moamenmahmoud118@gmail.com>
2026-05-27 02:19:11 +0300

    Add booking lookup by reference with related entities
    Implemented GetBookingByRefrenceAsync in repository and service interfaces to fetch bookings by reference, including related entities. Introduced BookingByRefrenceSpecification to encapsulate query logic with necessary includes.

--- Ticketa.Infrastructure/Repositories/BookingRepository.cs
+++ Ticketa.Infrastructure/Repositories/BookingRepository.cs
@@ -1,10 +1,16 @@
 ﻿using Ticketa.Core.Entities;
 using Ticketa.Core.Interfaces.IRepositories;
+using Ticketa.Core.Specifications;
 using Ticketa.Infrastructure.Data;
 
 namespace Ticketa.Infrastructure.Repositories
 {
   public class BookingRepository(ApplicationDbContext context) : GenericRepository<Booking>(context), IBookingRepository
   {
+    public async Task<Booking?> GetBookingByRefrenceAsync(string reference, CancellationToken ct = default)
+    {
+      var spec = new BookingByRefrenceSpecification(reference);
+      return await GetEntityWithSpecAsync(spec);
+    }
   }
 }
