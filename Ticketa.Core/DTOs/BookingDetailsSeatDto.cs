936c9055182a30dbb9e35de645831d21436c54f0
moamenFathy <moamenmahmoud118@gmail.com>
2026-05-27 02:18:44 +0300

    Add BookingDetailsDto and BookingDetailsSeatDto classes
    Introduce DTOs for detailed booking info, including user, movie, hall, and seat details. BookingDetailsDto aggregates booking data and a list of BookingDetailsSeatDto, which holds individual seat information.

---
+++ Ticketa.Core/DTOs/BookingDetailsSeatDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticketa.Core.DTOs
+{
+  public class BookingDetailsSeatDto
+  {
+    public int Row { get; set; }
+    public int SeatNumber { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+  }
+}
