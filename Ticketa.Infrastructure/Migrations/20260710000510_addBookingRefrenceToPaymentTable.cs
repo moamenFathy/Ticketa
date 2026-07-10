f1c91b744143d5469873360ccc17e88cbea598d9
moamenFathy <moamenmahmoud118@gmail.com>
2026-07-10 03:32:18 +0300

    Add BookingReference to Payment entity and table
    Added a nullable BookingReference property to the Payment entity. Updated the database schema via EF Core migration to include the new column in the Payments table. Updated model snapshot and migration designer files accordingly.

---
+++ Ticketa.Infrastructure/Migrations/20260710000510_addBookingRefrenceToPaymentTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBookingRefrenceToPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookingReference",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookingReference",
+                table: "Payments");
+        }
+    }
+}
