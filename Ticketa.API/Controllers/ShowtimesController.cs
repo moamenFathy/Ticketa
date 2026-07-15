5db8d375f89ec2d17c9dc91979b613d72d5e0f05
moamenmahmoud118@gmail.com <moamenmahmoud118@gmail.com>
2026-07-15 23:23:19 +0300

    Require auth for showtimes; update booking QR code URL
    ShowtimesController endpoints now require authorization.
    Changed QR code URL in booking confirmation from /scan/{ref}
    to /bookings/{ref} for improved consistency.

--- Ticketa.API/Controllers/ShowtimesController.cs
+++ Ticketa.API/Controllers/ShowtimesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Ticketa.Core.DTOs;
 using Ticketa.Core.Interfaces.IServices;
 
@@ -6,6 +7,7 @@ namespace Ticketa.API.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
+  [Authorize]
   public class ShowtimesController(IShowtimeService showtimeService) : ControllerBase
   {
     private readonly IShowtimeService _showtimeService = showtimeService;
